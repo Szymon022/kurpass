f47dd55fe04b256115e8a67b989983975cc57067
Szymon022 <szymon.kurpas00@gmail.com>
2023-03-13 11:36:29 +0100

    Initial commit

---
+++ TDD/StringCalculator.cs
@@ -0,0 +1,38 @@
+﻿
+namespace TDD
+{
+	public class StringCalculator
+	{
+		public int Calculate(string arg)
+		{
+			if (arg == "") return 0;
+
+			var isNumeric = int.TryParse(arg, out int n);
+			if(isNumeric && n>=0 && n<=1000) return n;
+			if (isNumeric && n > 1000) return 0;
+			if(isNumeric && n<0) throw new ArgumentOutOfRangeException();
+
+			char[] delimiters = { ',', '\n' };
+			if (arg[0] == '/' && arg[1] == '/')
+			{
+				delimiters.Append(arg[2]);
+				arg = arg.Remove(0, 3);
+			}
+
+			var words = arg.Split(delimiters);
+			int[] values = new int[words.Length];
+
+			int finalValue = 0;
+			for (int i = 0; i < words.Length; i++)
+			{
+				int.TryParse(words[i], out values[i]);
+				if (values[i] <= 1000)
+				{
+					finalValue += values[i];
+				}
+			}
+
+			return finalValue;
+		}
+	}
+}
---
+++ TDDTests/StringCalculatorTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Principal;
+
+
+namespace TDD.Tests
+{
+	[TestClass()]
+	public class StringCalculatorTests
+	{
+		[TestMethod()]
+		public void Returns0ForEmptyStringArgument()
+		{
+			StringCalculator calculator= new StringCalculator();
+			int value = calculator.Calculate("");
+			int valueExpected = 0;
+			Assert.AreEqual(valueExpected,value);
+		}
+
+		[TestMethod()]
+		public void ReturnsValueForSingleNumberString()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("23");
+			int valueExpected = 23;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void ReturnsSumForTwoNumbersDelimitedByComa()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("20,5");
+			int valueExpected = 25;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void ReturnsSumForTwoNumbersDelimitedByNewline()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("10\n5");
+			int valueExpected = 15;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void ReturnsSumForThreeNumbersDelimitedByComa()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("10,5,1");
+			int valueExpected = 16;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void ReturnsSumForThreeNumbersDelimitedByNewline()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("10\n5\n1");
+			int valueExpected = 16;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void ThrowsErrorForNegativeNumber()
+		{
+			StringCalculator calculator = new StringCalculator();
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => calculator.Calculate("-2"));
+		}
+
+		[TestMethod()]
+		public void IgnoresValuesGreaterThan1000()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("1001");
+			int valueExpected = 0;
+			Assert.AreEqual(valueExpected, value);
+		}
+
+		[TestMethod()]
+		public void NewDelimiter()
+		{
+			StringCalculator calculator = new StringCalculator();
+			int value = calculator.Calculate("//$1,2$3\n4");
+			int valueExpected = 10;
+			Assert.AreEqual(valueExpected, value);
+		}
+	}
+}
